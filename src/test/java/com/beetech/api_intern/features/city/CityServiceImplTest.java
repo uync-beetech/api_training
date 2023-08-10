bd772e02c47ddb3644b4ddf989230c4325dbe112
uync-beetech <uy.nc@beetechsoft.vn>
2023-08-10 15:39:21 +0700

    create unit test for city and district service

--- src/test/java/com/beetech/api_intern/features/city/CityServiceImplTest.java
+++ src/test/java/com/beetech/api_intern/features/city/CityServiceImplTest.java
@@ -1,4 +1,55 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.beetech.api_intern.features.city;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class CityServiceImplTest {
-  
+    @Mock
+    CityRepository cityRepository;
+
+    @Mock
+    CityService cityService;
+
+    @BeforeEach
+    void setUp() {
+        cityRepository = mock(CityRepository.class);
+        cityService = new CityServiceImpl(cityRepository);
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findAll_HasRecordsInDatabase_ShouldReturnListCity() {
+        List<City> cities = new ArrayList<>();
+        for (int i = 1; i < 5; i++) {
+            cities.add(City.builder()
+                    .id(i)
+                    .name("Test city " + i)
+                    .build());
+        }
+        when(cityRepository.findAll()).thenAnswer(invocationOnMock -> cities);
+
+        assertFalse(cityService.findAll().isEmpty());
+    }
+
+    @Test
+    void findAll_HasNoRecordsInDatabase_ShouldReturnEmptyList() {
+        when(cityRepository.findAll()).thenAnswer(invocationOnMock -> new ArrayList<City>());
+        assertTrue(cityService.findAll().isEmpty());
+    }
 }
--- src/test/java/com/beetech/api_intern/features/district/DistrictServiceImplTest.java
+++ src/test/java/com/beetech/api_intern/features/district/DistrictServiceImplTest.java
@@ -1,4 +1,61 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.beetech.api_intern.features.district;
+
+import com.beetech.api_intern.features.city.City;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class DistrictServiceImplTest {
-  
+    @Mock
+    DistrictRepository districtRepository;
+
+    @Mock
+    DistrictService districtService;
+
+    List<District> districts;
+
+    @BeforeEach
+    void setUp() {
+        districtRepository = mock(DistrictRepository.class);
+        districtService = new DistrictServiceImpl(districtRepository);
+        districts = new ArrayList<>();
+        for (int i = 1; i < 10; i++) {
+            districts.add(District.builder()
+                    .id(i)
+                    .city(City.builder().id(1).name("Test city").build())
+                    .name("Test district " + i)
+                    .build());
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findAll_HasRecordsInDatabase_ShouldReturnListDistrict() {
+        when(districtRepository.findAllByCityId(any(Integer.class)))
+                .thenAnswer(invocationOnMock -> districts);
+        assertFalse(districtService.findAll(1).isEmpty());
+    }
+
+    @Test
+    void findAll_HasNoRecordsInDatabase_ShouldReturnEmptyList() {
+        when(districtRepository.findAllByCityId(any(Integer.class)))
+                .thenAnswer(invocationOnMock -> new ArrayList<District>());
+        assertTrue(districtService.findAll(1).isEmpty());
+    }
 }
